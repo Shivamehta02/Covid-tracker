f940f4338d08fc3e83a70f747812768972b578e7
Shiva Mehta <shivamehta73@gmail.com>
2021-09-20 21:51:04 +0530

    added heroku deployement

--- covidtracker/settings.py
+++ covidtracker/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'vd2ki-h6*6rsy0kub%^mv1wqb=k*-sm-jsultrp41_v2h$awth'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','covid-tracker-shiva.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,4 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR/"static"
+]
